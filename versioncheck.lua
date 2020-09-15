4381f90a113b2800f5a916928d3ecabd8820d625
Eduardo Costa Scott <edu.scott01@gmail.com>
2020-09-15 20:16:47 -0300

    Add files via upload

---
+++ versioncheck.lua
@@ -0,0 +1,14 @@
+Citizen.CreateThread( function()
+resourceName = "Scene Menu ("..GetCurrentResourceName()..")" -- the resource name
+
+function checkVersion(err,responseText, headers)
+	curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
+	if curVersion ~= responseText then
+		print("^1"..resourceName.." is outdated.^7 \nShould be: "..responseText.."\nYour Version: "..curVersion.."\nPlease update it at https://github.com/IFLStud/scenemenu")
+	else
+		print("\n"..resourceName.." is up to date, have fun!")
+	end
+end
+
+PerformHttpRequest("https://raw.githubusercontent.com/IFLStud/scenemenu/master/scenemenu/version", checkVersion, "GET")
+end)
